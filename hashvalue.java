7b6222350561b05bcc4b76fe9b1f3dc187ba2dd8
chava manish <52528480+Chavamanish@users.noreply.github.com>
2021-10-13 12:42:49 +0530

    Add files via upload

---
+++ hashvalue.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class hashvalue {
+    public static void main(String args[]){
+        String s1= sc.next();
+        String s2= sc.next();
+        System.out.println("string s1 hashcode"+s1.hashCode());
+        System.out.println("String s2 hashcode"+s2.hashCode());
+    }
+    
+}
